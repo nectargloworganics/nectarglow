a92de5d4a9ecee4bb198ec0f17b0ee921928eb87
nectargloworganics <nectargloworganics@gmail.com>
2026-01-22 20:10:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ app.get("/", (_, res) => {
   res.send("NectarGlow API running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
